a29826518d8e64d2c6fb11b867a55e4e8fcc571e
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2022-11-02 09:28:43 +0000

    Add files via upload

---
+++ DayOne/assignment.py
@@ -0,0 +1,41 @@
+favourite_number = 4
+
+favourite_number += 10
+# favourite_number = favourite_number + 10
+# what is favourite number equal to?
+# 14
+favourite_number + 50 # favourite_number only changes to 64 ON this line... and this line only!
+print(favourite_number) # what is the value of favourite_number on line 8
+
+favourite_number -= 5 # favourite_number = 14 - 5
+# favourite_number = favourite_number - 5
+# what is favourite number equal to?
+# 9
+print(favourite_number)
+
+favourite_number *= 3 # favourite_number = 9 * 3
+# favourite_number = favourite_number * 3
+# what is favourite number equal to?
+# 27
+print(favourite_number)
+
+favourite_number /= 9 # favourite_number = 27 / 9
+# favourite_number = favourite_number / 9
+# what is favourite number equal to?
+# 3
+print(favourite_number)
+
+mean_number = 150
+
+mean_number += 40 # 190
+mean_number - 5 # 190
+mean_number += 10 # 200
+mean_number / 4 # 200
+mean_number /= 4 # 50
+mean_number -= 10 # 40
+mean_number - 10 # 40
+mean_number * 10 # 40
+mean_number *= 5 # 200
+print(mean_number)
+# what is the value of mean_number at the end?
+# what is the value of mean_number at all stages?
